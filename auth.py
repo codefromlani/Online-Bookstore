18d9ce03da019c0c7f61b17b3b61497c94eab0f0
Rodiat Hammed <rodiathammed48@gmail.com>
2024-12-11 15:55:06 +0100

    add admin authorization required

--- auth.py
+++ auth.py
@@ -65,3 +65,10 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     if user is None:
         raise credentials_exception
     return user
+
+def admin_required(current_user: models.User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don't have permission to perform this action"
+            )
